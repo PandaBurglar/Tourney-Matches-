dd9377f7183bccfcfa0c3f6201c365a48ba75ab9
root <root@PANDABURGLAR99.localdomain>
2022-01-18 20:32:20 -0500

    working app

---
+++ src/components/PlayerList.js
@@ -0,0 +1,16 @@
+import Player from './Player';
+
+function PlayerList(props){
+  const { playerData } = props;
+  // player={player} is same as {...player}
+  console.log(playerData);
+  const parsedPlayers = playerData.map(player => <Player key={player.gamerTag} {...player} />);
+  return (
+    <section className='PlayerList'>
+    <h1>Current participating players</h1>
+    { parsedPlayers }
+  </section>
+  );
+}
+
+export default PlayerList;
